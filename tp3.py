0200903f6cdc455d039e1a6f50ee369b0d47eb76
Santino Demaría <demarias@udesa.edu.ar>
2024-05-27 15:27:27 -0300

    Palu UWU 7w7 *3*

--- tp3.py
+++ tp3.py
@@ -1,1 +1,20 @@
-# Hello world
+# Haz una funcion de factorizacion PA=LU
+
+import numpy as np
+
+def factorizacionLU(A):
+    n = len(A)
+    L = np.eye(n)
+    U = np.zeros((n,n))
+    for i in range(n):
+        for j in range(i,n):
+            sum = 0
+            for k in range(i):
+                sum += L[i][k]*U[k][j]
+            U[i][j] = A[i][j] - sum
+        for j in range(i+1,n):
+            sum = 0
+            for k in range(i):
+                sum += L[j][k]*U[k][i]
+            L[j][i] = (A[j][i] - sum)/U[i][i]
+    return L, U
