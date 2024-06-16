de6d5663886a48ca7d19f0b98aa50d72dca59dd3
totobena28 <benavideztomas28@gmail.com>
2024-06-16 15:01:30 -0300

    tp4

--- TP4/tp4.py
+++ TP4/tp4.py
@@ -1,41 +1,3 @@
-'''
-1 Cuadrados mínimos mediante descenso por gradiente
-El objetivo de este trabajo es aplicar el algoritmo de gradiente descendente al problema de encontrar la
-solución del sistema
-Ax = b, (1)
-donde A ∈ R
-n×d
-, x ∈ R
-d y b ∈ R
-n
-. Para hacer esto primero definimos la función de costo
-F(x) = (Ax − b)
-T
-(Ax − b). (2)
-El algoritmo de gradiente descendente busca encontrar x
-∗
-, la solución que minimiza F, mediante el proceso
-iterativo
-xk+1 = xk − s∇F(xk), (3)
-donde s es el paso utilizado.
-Cuando el problema tiene más incógnitas que ecuaciones se suelen aplicar regularizaciones que inducen
-alguna propiedad deseada en la solución obtenida. Una variante típica es agregarle un término que dependa
-de la norma-2 del vector al cuadrado a la función de costo, lo que resulta en
-F2(x) = F(x) + δ2∥x∥
-2
-2
-, (4)
-donde δ2 es un nuevo parámetro a elegir. Esto se conoce como regularización L2. Algunas definiciones antes
-de pasar a las consignas: σ son los valores principales de A, λ son los autovalores de H, donde H es el
-Hessiano de F (no de F2).
-Objetivos:
-• Tomando n = 5 y d = 100 genere matrices A y vectores b aleatorios y resuelva el problema minimizando
-F y F2. Tome δ2 = 10−2σmax cuando trabaje con F2. En todos los casos utilice s = 1/λmax,
-una condición inicial aleatoria y realice 1000 iteraciones. Estudiar como evoluciona la solución del
-gradiente descendiente iterativamente. Compare con la solución obtenida mediante SVD. Analice los
-resultados. ¿Por qué se elige este valor de s? ¿Qué sucede si se varían los valores de δ2?
-'''
-
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg as s
@@ -81,12 +43,35 @@ def main():
 
     #graficos
     plt.figure()
+    plt.title('Comparación de soluciones')
     plt.plot(x, label='Gradiente descendente')
-    plt.plot(x_reg, label='Gradiente descendente regularizado')
+    # plt.plot(x_reg, label='Gradiente descendente regularizado')
     plt.plot(x_svd, label='SVD')
     # plt.plot(x_least_squares, label='Cuadrados mínimos')
+    plt.xlabel('x')
+    plt.yscale('log')    
+    plt.ylabel('y')
     plt.legend()
     plt.show()
+
+    #variar delta
+    deltas = np.logspace(-5, 0, 6)
+    errores = []
+    for delta in deltas:
+        x_reg = gradiente_descendente_reg(A, b, s, x0, n_iter, delta)
+        errores.append(np.linalg.norm(np.dot(A, x_reg) - b))
+    plt.figure()
+    plt.title('Error en función de delta')
+    plt.plot(deltas, errores)
+    plt.xlabel('delta')
+    plt.ylabel('Error')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.show()
+    
+
+#falta el Hesiano, verlo en OneNote
+
 
 
 if __name__ == '__main__':
