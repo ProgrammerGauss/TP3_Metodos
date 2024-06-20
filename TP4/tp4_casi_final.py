2c57ef1e57b241830a10a4c3196354bc0e3ce828
ProgrammerGauss <127429460+ProgrammerGauss@users.noreply.github.com>
2024-06-20 05:12:53 -0300

    borre unas listas que no se usaban

--- TP4/tp4_casi_final.py
+++ TP4/tp4_casi_final.py
@@ -14,7 +14,7 @@ sigma_max = np.linalg.svd(A, compute_uv=False)[0]
 delta2 = 1e-2 * sigma_max
 lambda_max = np.linalg.eigvals(H_F).real.max()
 s = 1 / lambda_max
-iterations = 5000
+iterations = 1000
 
 
 # Función de costo F(x)
@@ -168,13 +168,9 @@ def relative_error(x_iter):
 def grafico_error_relativo():
     x = x_init.copy()
     history_F = []
-    history_norm_x = []
-    history_residual = []
 
     x_reg = x_init.copy()
     history_F_reg = []
-    history_norm_x_reg = []
-    history_residual_reg = []
 
     for _ in range(iterations):
         grad = grad_F(x)
@@ -182,16 +178,12 @@ def grafico_error_relativo():
             break
         x = x - s * grad
         history_F.append(F(x))
-        history_norm_x.append(np.linalg.norm(x) ** 2)
-        history_residual.append(np.linalg.norm(A @ x - b) ** 2)
 
         grad_reg = grad_F2(x_reg, delta2)
         if np.any(np.isnan(grad)) or np.any(np.isinf(grad)):
             break
         x_reg = x_reg - s * grad_reg
         history_F_reg.append(F2(x_reg, delta2))
-        history_norm_x_reg.append(np.linalg.norm(x_reg) ** 2)
-        history_residual_reg.append(np.linalg.norm(A @ x_reg - b) ** 2)
 
     relative_errors = [relative_error(x) for x in history_F]
     relative_errors_reg = [relative_error(x) for x in history_F_reg]
